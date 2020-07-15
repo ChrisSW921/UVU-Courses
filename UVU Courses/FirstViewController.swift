048db34e1c68cace6643fb236c65ea1bdfc06139
Chris <withscottc921@gmail.com>
2020-07-15 07:25:07 -0600

    Initial Commit

---
+++ UVU Courses/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  UVU Courses
+//
+//  Created by Chris Withers on 7/15/20.
+//  Copyright © 2020 Chris Withers. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ UVU CoursesTests/UVU_CoursesTests.swift
@@ -0,0 +1,34 @@
+//
+//  UVU_CoursesTests.swift
+//  UVU CoursesTests
+//
+//  Created by Chris Withers on 7/15/20.
+//  Copyright © 2020 Chris Withers. All rights reserved.
+//
+
+import XCTest
+@testable import UVU_Courses
+
+class UVU_CoursesTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
