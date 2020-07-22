0698d48e0d72a19909d9faee3d28d10297605983
Chris <withscottc921@gmail.com>
2020-07-22 08:03:47 -0600

    Added Review Struct

---
+++ UVU Courses/Models/Review.swift
@@ -0,0 +1,20 @@
+//
+//  Review.swift
+//  UVU Courses
+//
+//  Created by Chris Withers on 7/22/20.
+//  Copyright © 2020 Chris Withers. All rights reserved.
+//
+
+import Foundation
+
+struct Review {
+    var CourseName: String = ""
+    var DateTaken: String = ""
+    var Professor: String = ""
+    var Books: String = ""
+    var Homework: String = ""
+    var Exams: String = ""
+    var Attendance: Bool = true
+    var User: String = ""
+}
